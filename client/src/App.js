a5fff430b67dcc7316ab3272c4d5707d9685b49c
unknown <mudasar.se@gmail.com>
2019-08-09 11:29:12 +0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Alert from "./components/layout/Alert";
-import { loadUser } from "./Redux/Actions/auth";
-import setAuthToken from "./util/setAuthToken";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
-import PrivateRoute from "./components/routing/PrivateRoute";
+import Routes from './components/routing/Routes'
+
 //Redux
 import { Provider } from "react-redux";
+import { loadUser } from "./Redux/Actions/auth";
 import store from "./Redux/Store";
+import setAuthToken from "./util/setAuthToken";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -36,47 +25,11 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
+          <Switch>
           <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:id" component={Profile} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute
-                exact
-                path="/create-profile"
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/edit-profile"
-                component={EditProfile}
-              />
-               <PrivateRoute
-                exact
-                path="/add-experience"
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path="/add-education"
-                component={AddEducation}
-              />
-              <PrivateRoute
-                exact
-                path="/posts"
-                component={Posts}
-              />
-               <PrivateRoute
-                exact
-                path="/posts/:id"
-                component={Post}
-              />
-            </Switch>
-          </section>
+           <Route component = { Routes } />
+          </Switch>
+         
         </Fragment>
       </Router>
     </Provider>
